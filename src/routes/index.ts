8f843bd825e4237406de11d3beb59964c1e15859
wangwei <335968838@qq.com>
2025-06-08 17:13:34 +0800

    feat: 修改路由配置

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHashHistory, createRouter } from "vue-router";
 
 const routes = [
   {
@@ -14,6 +14,6 @@ const routes = [
 ];
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
